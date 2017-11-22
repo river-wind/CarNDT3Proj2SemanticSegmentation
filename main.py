dd723a079630f048730e66ccc10e4ef75f0457b3
river-wind <river_wind@mac.com>
2017-11-21 21:17:42 -0500

    learning rate back under 0.001

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op,
     for epoch in range(epochs):
       for z, (image, label) in enumerate(get_batches_fn(batch_size)):
         # Training
-        _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image:image, correct_label:label, keep_prob:0.4, keep_prob: 0.5, learning_rate: 0.001}) 
+        _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image:image, correct_label:label, keep_prob:0.4, keep_prob: 0.5, learning_rate: 0.0008}) 
 
         print("epoch: {}  batch: {}  loss: {}".format(epoch+1, z, loss))
       
